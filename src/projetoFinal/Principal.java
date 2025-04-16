09a0d91a154f130b815b03381bbb4cdce098fd25
EduardoDias1902 <eduardo_d_maia@estudante.sesisenai.org.br>
2025-04-15 21:36:41 -0300

    Título acrescentado

--- src/projetoFinal/Principal.java
+++ src/projetoFinal/Principal.java
@@ -3,7 +3,17 @@ package projetoFinal;
 public class Principal {
 
 	public static void main(String[] args) {
-		
+		System.out.println("		┌────────────────────────────────────────────────────────────────────────────────────────────────────────┐");
+		System.out.println("		│I8,        8        ,8I  88888888888  ,ad8888ba,           ,ad8888ba,                                   │");
+		System.out.println("		│ 8b       d8b       d8   88          d8        8b         d8        8b                                  │");
+		System.out.println("		│  8,     ,8 8,     ,8    88         d8'                  d8'        `8b                                 │");
+		System.out.println("		│  Y8     8P Y8     8P    88aaaaaa   88                   88          88  8b,dPPYba,    ,adPPYba,        │");
+		System.out.println("		│  `8b   d8' `8b   d8'    88         88      88888        88          88  88P'     8a  a8P_____88        │");
+		System.out.println("		│   `8a a8'   `8a a8'     88         Y8,        88        Y8,        ,8P  88       88  8PP               │");
+		System.out.println("		│    `8a8'     `8a8'      88          Y8a.    .a88         Y8a.    .a8P   88       88   8b   ,aa         │");
+		System.out.println("		│      8         8        88888888888  `AY88888P              Y8888Y      88       88     Ybbd8          │");
+		System.out.println("		└────────────────────────────────────────────────────────────────────────────────────────────────────────┘");
+
 
 	}
 
