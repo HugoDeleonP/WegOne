2913a416c33bbc6f39b3a93c6e041bf1b438077c
Hugo <hugo_paim@estudante.sesisenai.org.br>
2025-05-26 12:28:33 -0300

    feat: Criação da classe ConnectionDB e sua funcionalidade inicial.

---
+++ src/projetoFinal/ConnectionDB.java
@@ -0,0 +1,19 @@
+package projetoFinal;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.sql.Connection;
+
+public class ConnectionDB {
+    private static final String URL = "jdbc:mysql://root:beQwLQrVagQlJWUjWkSvIMrrTtOhiXuW@ballast.proxy.rlwy.net:43059/railway";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "beQwLQrVagQlJWUjWkSvIMrrTtOhiXuW";
+    public static Connection getConnection(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException("Erro ao conectar com o banco de dados", e);
+        }
+
+    }
+}
